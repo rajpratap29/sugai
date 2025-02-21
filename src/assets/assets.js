0db712511cdc5890e3c35e2fccb83c7316000840
rajpratap29 <therajpratapofficial@gmail.com>
2025-02-21 22:17:37 +0530

    Finished homepage and media query

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,5 +1,25 @@
 import logo from "./logo.png"
+import arrow from "./arrow.png"
+import attachment from "./attachment.png"
+import bg from "./bg.png"
+import bot from "./bot.png"
+import chat from "./chat.png"
+import code from "./code.png"
+import image from "./image.png"
+import orbital from "./orbital.png"
+import human1 from "./human1.jpg"
+import human2 from "./human2.jpg"
 
 export const assets = {
-    logo
+    logo,
+    arrow,
+    attachment,
+    bg,
+    bot,
+    chat,
+    code,
+    image,
+    orbital,
+    human1,
+    human2
 }
--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -1,10 +1,74 @@
 import { Link } from "react-router-dom";
 import "./homepage.css";
+import { assets } from "../../assets/assets";
+import { TypeAnimation } from "react-type-animation";
+import { useState } from "react";
 
 const Homepage = () => {
-  return <div className="homepage">
-    <Link to="/dashboard">Dashboard</Link>
-  </div>;
+
+  const [typingStatus, setTypingStatus] = useState("human1")
+
+  return (
+    <div className="homepage">
+      <img src={assets.orbital} alt="" className="orbital" />
+      <div className="left">
+        <h1>SUG AI</h1>
+        <h2>Learn Smarter, Not Harder—Experience SUG AI!</h2>
+        <h3>
+          Upgrade your skills with AI-driven explanations and insights. Fuel
+          your curiosity with intelligent responses in seconds.
+        </h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src={assets.bot} alt="" className="bot" />
+          <div className="chat">
+            <img src={typingStatus === "human1" ? assets.human1 : typingStatus === "human2" ? assets.human2 : assets.bot} alt="" />
+            <TypeAnimation
+              sequence={[
+                "Raj: What is a closure in JavaScript?",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "SUG AI: A function with access to its outer scope even after execution.",
+                2000,
+                () => {
+                  setTypingStatus("human2");
+                },
+                "Priyavarat: What is JSX in React?",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "SUG AI: JSX lets you write HTML inside JavaScript.",
+                2000,
+                () => {
+                  setTypingStatus("human1");
+                },
+              ]}
+              wrapper="span"
+              repeat={Infinity}
+              cursor={true}
+              omitDeletionAnimation={true}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="terms">
+        <img src={assets.logo} alt="" />
+        <div className="links">
+          <Link to="/">Terms of Service</Link>
+          <span>|</span>
+          <Link to="/">Privacy Policy</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;

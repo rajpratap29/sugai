59b9cad3789ff2c6ada6dee8cdce1f999a8c4da7
rajpratap29 <therajpratapofficial@gmail.com>
2025-03-10 20:20:44 +0530

    Update: Made changes in rootLayout Model

--- frontend/src/layout/rootLayout/RootLayout.jsx
+++ frontend/src/layout/rootLayout/RootLayout.jsx
@@ -1,14 +1,9 @@
 import { Link, Outlet } from "react-router-dom";
 import "./rootLayout.css";
 import { assets } from "../../assets/assets";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/clerk-react";
+import { ClerkProvider, SignedIn, UserButton } from "@clerk/clerk-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 // console.log("Clerk Publishable Key:", PUBLISHABLE_KEY);
 
@@ -16,28 +11,29 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+const queryClient = new QueryClient();
+
 const RootLayout = () => {
   return (
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/" >
-      <div className="rootLayout">
-        <header>
-          <Link to="/" className="logo">
-            <img src={assets.logo} alt="" />
-            <span>SUG AI</span>
-          </Link>
-          <div className="user">
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </div>
-        </header>
-        <main>
-          <Outlet />
-        </main>
-      </div>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <QueryClientProvider client={queryClient}>
+        <div className="rootLayout">
+          <header>
+            <Link to="/" className="logo">
+              <img src={assets.logo} alt="" />
+              <span>SUG AI</span>
+            </Link>
+            <div className="user">
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </header>
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 };

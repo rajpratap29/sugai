e36ab154cf840d6f09a5d01fb1e4d51358dfd0a4
rajpratap29 <therajpratapofficial@gmail.com>
2025-03-10 20:20:18 +0530

    Update: Made changes in chatList Model

--- frontend/src/components/chatList/ChatList.jsx
+++ frontend/src/components/chatList/ChatList.jsx
@@ -1,38 +1,46 @@
-import { Link } from "react-router-dom"
-import "./chatList.css"
-import {assets} from "../../assets/assets"
+import { Link } from "react-router-dom";
+import "./chatList.css";
+import { assets } from "../../assets/assets";
+import { useQuery } from "@tanstack/react-query";
 
 const ChatList = () => {
-    return (
-        <div className="chatList">
-            <span className="title">DASHBOARD</span>
-            <Link to="/dashboard">New Chat</Link>
-            <Link to="/">Explore SUG AI</Link>
-            <Link to="/">Contact</Link>
-            <hr/>
-            <span className="title">RECENT CHATS</span>
-                <div className="list">
-                    <Link to="/">My chat test</Link>
-                    <Link to="/">My chat test</Link>
-                    <Link to="/">My chat test</Link>
-                    <Link to="/">My chat test</Link>
-                    <Link to="/">My chat test</Link>
-                    <Link to="/">My chat test</Link>
-                    <Link to="/">My chat test</Link>
-                    <Link to="/">My chat test</Link>
-                    <Link to="/">My chat test</Link>
-                    <Link to="/">My chat test</Link>
-                </div>
-            <hr/>
-            <div className="upgrade">
-                <img src={assets.logo} alt="" />
-                <div className="texts">
-                    <span>Upgrade to SUG AI Pro</span>
-                    <span>Get unlimited access to all features</span>
-                </div>
-            </div>
+  const { isPending, error, data } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: () =>
+      fetch(`${import.meta.env.VITE_API_URL}/api/userchats`, {
+        credentials: "include",
+      }).then((res) => res.json()),
+  });
+
+  return (
+    <div className="chatList">
+      <span className="title">DASHBOARD</span>
+      <Link to="/dashboard">New Chat</Link>
+      <Link to="/">Explore SUG AI</Link>
+      <Link to="/">Contact</Link>
+      <hr />
+      <span className="title">RECENT CHATS</span>
+      <div className="list">
+        {isPending
+          ? "Loading..."
+          : error
+          ? "Something went wrong"
+          : data?.map((chat) => (
+              <Link to={`/dashboard/chats/${chat._id}`} key={chat._id}>
+                {chat.title}
+              </Link>
+            ))}
+      </div>
+      <hr />
+      <div className="upgrade">
+        <img src={assets.logo} alt="" />
+        <div className="texts">
+          <span>Upgrade to SUG AI Pro</span>
+          <span>Get unlimited access to all features</span>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default ChatList;

330c8e4539512796bed381db17f15d956fe76ec9
rajpratap29 <therajpratapofficial@gmail.com>
2025-03-10 20:09:33 +0530

    Update: Added database functionality to backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,31 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import ImageKit from "imagekit";
+import mongoose from "mongoose";
+import Chat from "./models/chat.js";
+import UserChats from "./models/userChats.js";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-}))
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
@@ -20,6 +38,107 @@ app.get("/api/upload", (req, res) => {
   res.send(result);
 });
 
+// app.get("/api/test", requireAuth(), (req, res) => {
+//   const userId = req.auth.userId;
+//   console.log(userId);
+//   res.send("Success!");
+// });
+
+app.post("/api/chats", requireAuth(), async (req, res) => {
+  const userId = req.auth.userId;
+  const { text } = req.body;
+
+  try {
+    // CREATE A NEW CHAT
+    const newChat = new Chat({
+      userId: userId,
+      history: [{ role: "user", parts: [{ text }] }],
+    });
+
+    const savedChat = await newChat.save();
+
+    // CHECK IF THE USER CHATS EXIST
+    const userChats = await UserChats.find({ userId: userId });
+
+    // IF DOESN'T EXIST CREATE A NEW ONE AND ADD THE CHAT IN THE CHATS ARRAY
+    if (!userChats.length) {
+      const newUserChats = new UserChats({
+        userId: userId,
+        chats: [
+          {
+            _id: savedChat._id,
+            title: text.substring(0, 40),
+          },
+        ],
+      });
+
+      await newUserChats.save();
+    } else {
+      // IF EXISTS, PUSH THE CHATS TO THE EXISTNG ARRAY
+      await UserChats.updateOne(
+        { userId: userId },
+        {
+          $push: {
+            chats: {
+              _id: savedChat._id,
+              title: text.substring(0, 40),
+            },
+          },
+        }
+      );
+
+      res.status(201).send(newChat._id);
+    }
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .send(
+        "Error while creating chat! if you are developer of the website check line 42 to till arround 90 in index.js under backend folder"
+      );
+  }
+});
+
+app.get("/api/userchats", requireAuth(), async (req, res) => {
+  const userId = req.auth.userId;
+
+  try {
+    const userChats = await UserChats.find({ userId: userId });
+
+    res.status(200).send(userChats[0].chats);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .send(
+        "Error while fetching userchat! if you are developer of the website check index.js under backend folder"
+      );
+  }
+});
+
+app.get("/api/chats/:id", requireAuth(), async (req, res) => {
+  const userId = req.auth.userId;
+
+  try {
+    const chat = await Chat.findOne({ _id: req.params.id, userId });
+
+    res.status(200).send(chat);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .send(
+        "Error while fetching chat! if you are developer of the website check index.js under backend folder"
+      );
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(401).send("Unauthenticated!");
+});
+
 app.listen(port, () => {
+  connect();
   console.log("Server running on 3000");
 });
